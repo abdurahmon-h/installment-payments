3b80a913a92e8b95aa62a53297a2716a02b68050
= <abdurrahmon111@gmail.com>
2025-05-17 00:45:24 +0500

    modified: разделил функции по отдельности

--- main.go
+++ main.go
@@ -1,60 +1,9 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"strings"
+	"installment-payments/internal/payments"
 )
-
-var conditions = []int{3, 6, 9, 12, 18, 24}
-
-type ProductConditions struct {
-	MainTerms     int
-	AddComissions int
-}
-
-var productData = map[string]ProductConditions{
-	"смартфон":  {9, 3},
-	"компьтер":  {12, 4},
-	"телевизор": {18, 5},
-}
-
-func isValidCondition(term int) bool {
-	for _, t := range conditions {
-		if t == term {
-			return true
-		}
-	}
-	return false
-}
-func TotalAmount(product string, amount float64, month int) (float64, error) {
-	product = strings.ToLower(strings.TrimSpace(product))
-	data, match := productData[product]
-	if !match {
-
-		return 0, errors.New("Неизвестный продукт")
-	}
-
-	if !isValidCondition(month) {
-		return 0, errors.New("срок рассрочки должен быть одним из этих: 3, 6, 9, 12, 18, 24 месяцев")
-	}
-
-	if month <= data.MainTerms {
-		return amount, nil
-	}
-
-	extraMonth := 0
-	for _, t := range conditions {
-		if t > data.MainTerms && t <= month {
-			extraMonth++
-		}
-
-	}
-
-	commission := float64(extraMonth) * float64(data.AddComissions)
-	total := amount + (amount * commission / 100)
-	return total, nil
-}
 
 func main() {
 
@@ -63,7 +12,7 @@ func main() {
 	term := 18
 	phone := 992938001313
 
-	total, err := TotalAmount(product, price, term)
+	total, err := payments.TotalAmount(product, price, term)
 	if err != nil {
 		fmt.Println("Ошибка:", err)
 		return
